b06c20fe5adbba796e63e3839638bbd58b086e70
Yftach Shmaryahu <yftachshm@gmail.com>
2020-11-27 14:12:25 +0200

    hooks example

--- with-hooks/src/App.js
+++ with-hooks/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+
+const getUrl = userId => {
+    if (userId.length === 0) return 'http://jsonplaceholder.typicode.com/posts';
+    return `http://jsonplaceholder.typicode.com/posts?userId=${userId}`;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [userId, setUserId] = useState('');
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        let isCanceled = false;
+        // TODO: Find and add public URL for the example
+        fetch(getUrl(userId))
+        .then(response => response.json())
+        .then(data => {
+            if (!isCanceled) {
+                console.log(`setting data userId=${userId}`);
+                setPosts(data);
+            }
+        })
+        .catch(console.error);
+
+        return () => isCanceled = true;
+    }, [userId]);
+
+    return (
+        <div className="App">
+            <input  className='search-term'
+                    value={userId}
+                    onChange={({ target }) => setUserId(target.value)}
+                    placeholder='Enter user ID...' />
+            <div className="results">
+                {
+                    posts.map(post => {
+                        const {
+                            id,
+                            title,
+                            body
+                        } = post;
+
+                        return (
+                            <div className='post' key={id}>
+                                <div className='title'>{title}</div>
+                                <div className='body'>{body}</div>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        </div>
+    );
 }
 
 export default App;
